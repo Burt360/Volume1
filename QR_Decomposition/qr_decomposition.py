b216d67dc4d1ca27f5840e588fb48010bdecf7fe
Nathan Schill <schilln@byu.edu>
2022-10-18 10:02:23 -0600

    Problems 1-5.

--- QR_Decomposition/qr_decomposition.py
+++ QR_Decomposition/qr_decomposition.py
@@ -1,10 +1,12 @@
 # qr_decomposition.py
 """Volume 1: The QR Decomposition.
-<Name>
-<Class>
-<Date>
+Nathan Schill
+Section 3
+Tues. Oct. 25, 2022
 """
 
+import numpy as np
+from scipy import linalg as la
 
 # Problem 1
 def qr_gram_schmidt(A):
@@ -17,7 +19,22 @@ def qr_gram_schmidt(A):
         Q ((m,n) ndarray): An orthonormal matrix.
         R ((n,n) ndarray): An upper triangular matrix.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    
+    ### Algorithm as given in Algorithm 3.1 in QR Decomposition PDF.
+
+    m, n = A.shape
+    Q = A.copy()
+    R = np.zeros((n, n))
+
+    for i in range(n):
+        R[i,i] = la.norm(Q[:,i])
+        Q[:,i] = Q[:,i]/R[i,i]
+
+        for j in range(i+1, n):
+            R[i,j] = np.dot(Q[:,j].T, Q[:,i])
+            Q[:,j] = Q[:,j] - R[i,j] * Q[:,i]
+
+    return Q, R
 
 
 # Problem 2
@@ -31,7 +48,9 @@ def abs_det(A):
     Returns:
         (float) the absolute value of the determinant of A.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    
+    ### det(A) as given in equation 3.1 in QR Decomposition PDF.
+    return np.abs(np.prod(np.diag(la.qr(A, mode='economic')[1])))
 
 
 # Problem 3
@@ -45,7 +64,23 @@ def solve(A, b):
     Returns:
         x ((n, ) ndarray): The solution to the system Ax = b.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    
+    m, n = A.shape
+    Q, R = la.qr(A, mode='economic')
+
+    y = Q.T @ b
+
+    # Solve Rx = y for x.
+
+    # Init x as row  for easier coding
+    x = [0] * n
+
+    ### Solve Ux = y for x
+    # Iterate up the entries of x
+    for i in range(n-1, 0-1, -1):
+        x[i] = 1/R[i][i] * (y[i] - sum([R[i][j] * x[j] for j in range(i+1, n)]))
+
+    return np.array(x)
 
 
 # Problem 4
@@ -59,7 +94,23 @@ def qr_householder(A):
         Q ((m,m) ndarray): An orthonormal matrix.
         R ((m,n) ndarray): An upper triangular matrix.
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    
+    ### Algorithm as given in Algorithm 3.2 in QR Decomposition PDF.
+    m, n = A.shape
+    Q = np.identity(m)
+    R = A.copy()
+
+    sign = lambda x: 1 if x >= 0 else -1
+
+    for k in range(n):
+        u = R[k:,k].copy()
+        u[0] = u[0] + sign(u[0]) * la.norm(u)
+        u = u/la.norm(u)
+
+        R[k:,k:] = R[k:,k:] - 2*np.outer(u, np.dot(u, R[k:,k:]))
+        Q[k:,:] = Q[k:,:] - 2*np.outer(u, np.dot(u, Q[k:,:]))
+
+    return Q.T, R
 
 
 # Problem 5
@@ -74,4 +125,23 @@ def hessenberg(A):
         H ((n,n) ndarray): The upper Hessenberg form of A.
         Q ((n,n) ndarray): An orthonormal matrix.
     """
-    raise NotImplementedError("Problem 5 Incomplete")
+
+    ### Algorithm as given in Algorithm 3.3 in QR Decomposition PDF.
+    
+    m, n = A.shape
+    H = A.copy()
+    Q = np.identity(m)
+
+    sign = lambda x: 1 if x >= 0 else -1
+
+    for k in range(n-2):
+        u = H[k+1:,k].copy()
+        u[0] = u[0] + sign(u[0]) * la.norm(u)
+
+        u = u/la.norm(u)
+
+        H[k+1:,k:] = H[k+1:,k:] - 2*np.outer(u, np.dot(u.T, H[k+1:,k:]))
+        H[:,k+1:] = H[:,k+1:] - 2*np.outer(np.dot(H[:,k+1:], u), u.T)
+        Q[k+1:,:] = Q[k+1:,:] - 2*np.outer(u, np.dot(u.T, Q[k+1:,:]))
+
+    return H, Q.T
